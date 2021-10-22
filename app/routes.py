b5f872c552e6d889568c355a15fe285f82512ac5
ayaka <ayakaob@gmail.com>
2021-10-21 22:29:19 -0700

    add GET route /books

--- app/routes.py
+++ app/routes.py
@@ -4,14 +4,28 @@ from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    new_book = Book(
-        title = request_body["title"],
-        description = request_body["description"]
-    )
-    db.session.add(new_book)
-    db.session.commit()
+    if request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(
+            title = request_body["title"],
+            description = request_body["description"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
 
-    return make_response(f"Book {new_book.title} created", 201)
+        return make_response(f"Book {new_book.title} created", 201)
+
+    elif request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append(
+                {
+                    "id": book.id,
+                    "title": book.title,
+                    "description": book.description
+                }
+            )
+        return jsonify(books_response)
